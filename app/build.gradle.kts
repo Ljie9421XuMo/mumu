3440cca4672cc6c0a4a111c79a94ed25567494c4
Ljie9421XuMo <1334694885@qq.com>
2026-09-12 07:39:07 +0800

    chore: versionCode 4 / versionName 0.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.mumu.pet"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.3.2"
+        versionCode = 4
+        versionName = "0.3.3"
 
         buildConfigField("String", "SUPABASE_URL", "\"$supabaseUrl\"")
         buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\"$supabaseKey\"")
